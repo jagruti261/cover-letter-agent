ba72c08c5ee7e4513b20c0b64e3f5db16e843820
Jagruti Vekariya <65525911+jagruti261@users.noreply.github.com>
2025-08-18 12:59:54 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,18 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
+// Set page title
+document.title = "CoverCraft - Crafting personalized cover letters that make you stand out";
+
+// Set favicon
+const link = document.createElement("link");
+link.rel = "icon";
+link.href = "/favicon.ico"; // path in public folder
+document.head.appendChild(link);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-     <BrowserRouter> <App /></BrowserRouter>
-   
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
