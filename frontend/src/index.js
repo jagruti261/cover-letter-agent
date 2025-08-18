35ec8514fca5ae2e05fdb840910d6efa8ddda04f
Jagruti Vekariya <65525911+jagruti261@users.noreply.github.com>
2025-08-18 13:43:30 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,10 +8,10 @@ import App from './App';
 document.title = "CoverCraft - Crafting personalized cover letters that make you stand out";
 
 // Set favicon
-const link = document.createElement("link");
-link.rel = "icon";
-link.href = "/CoverCraft-fav.png"; 
-document.head.appendChild(link);
+const iconLink = document.createElement("link");
+iconLink.rel = "stylesheet";
+iconLink.href = "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=stylus_laser_pointer";
+document.head.appendChild(iconLink);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
